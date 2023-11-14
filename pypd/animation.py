e09bf810a3f73f38db2cec1e39ee67899d56bf63
mark-hobbs <markhobbs91@gmail.com>
2023-11-14 14:23:19 +0000

    Simplifying the Animation class but further refactoring is required

--- pypd/animation.py
+++ pypd/animation.py
@@ -11,19 +11,11 @@ import matplotlib.animation as animation
 
 
 class Animation:
-
     def __init__(self, frequency=100, name=None):
         self.frequency = frequency
-        self.name = name
-
-        if self.name is None:
-            self.name = self._generate_animation_name()
-        
+        self.name = name or self._generate_animation_name()
         self.frames = []
         self.fig, self.ax = plt.subplots()
-
-        self.scatter = self.ax.scatter([], [], s=[], c=[], cmap="jet")
-        self.title = self.ax.set_title('')
 
     @staticmethod
     def _generate_animation_name():
@@ -32,81 +24,52 @@ class Animation:
 
     def save_frame(self, frame):
         """
-        Append a matplotlib.figure.Figure object to the self.frames list at a 
+        Append a matplotlib.figure.Figure object to the self.frames list at a
         frequency defined by self.frequency
         """
         self.frames.append(copy.deepcopy(frame))
 
-    def init(self):
+    def _set_axis_limits(self, i):
+        x_data, y_data = self._get_data_from_frame(i)
+        self.ax.set_xlim(min(x_data), max(x_data))
+        self.ax.set_ylim(min(y_data), max(y_data))
+
+    def _get_data_from_frame(self, i):
+        return self.frames[i].get_axes()[0].collections[0].get_offsets().T
+
+    def _set_scatter_data(self, current_scatter):
+        scatter = self.ax.scatter([], [], s=[], c=[], cmap="jet")
+        scatter.set_offsets(current_scatter.get_offsets())
+        scatter.set_sizes(current_scatter.get_sizes())
+        scatter.set_array(current_scatter.get_array())
+        return scatter
+
+    def _update(self, frame):
         """
-        Set up the initial state of the scatter plot
-        """        
-        # Automatically set axis limits based on the initial data
-        x_data = self.frames[0].get_axes()[0].collections[0].get_offsets()[:, 0]
-        y_data = self.frames[0].get_axes()[0].collections[0].get_offsets()[:, 1]
+        Update the scatter plot (self.scatter) for each frame in the animation
 
-        x_min, x_max = min(x_data), max(x_data)
-        y_min, y_max = min(y_data), max(y_data)
-
-        self.ax.set_xlim(x_min, x_max)
-        self.ax.set_ylim(y_min, y_max)
-
-    def update(self, frame):
-        """
-        Update the scatter plot for each frame in the animation
+        Notes
+        -----
+        Required signature: `def func(frame, *fargs) -> iterable_of_artists`
         """
         self.ax.clear()
-        
-        # Get the scatter plot from the current figure
         current_scatter = self.frames[frame].get_axes()[0].collections[0]
-
-        # Copy the data from the current scatter plot to the animation scatter plot
-        self.scatter = self.ax.scatter([], [], s=[], c=[], cmap="jet")
-        self.title = self.ax.set_title('')
-
-        # Automatically set axis limits based on the initial data
-        x_data = self.frames[0].get_axes()[0].collections[0].get_offsets()[:, 0]
-        y_data = self.frames[0].get_axes()[0].collections[0].get_offsets()[:, 1]
-
-        x_min, x_max = min(x_data), max(x_data)
-        y_min, y_max = min(y_data), max(y_data)
-
-        self.ax.set_xlim(x_min, x_max)
-        self.ax.set_ylim(y_min, y_max)
-
-        self.scatter.set_offsets(current_scatter.get_offsets())
-        self.scatter.set_sizes(current_scatter.get_sizes())
-        self.scatter.set_array(current_scatter.get_array())
-
-        self.title.set_text(f'Frame {frame}')
-
-        return self.scatter,
+        self._set_axis_limits(frame)
+        scatter = self._set_scatter_data(current_scatter)
+        self.ax.set_title(f"frame {frame}")
+        self.ax.set_aspect("equal", "box")
+        self.ax.axis("off")
+        return scatter
 
     def generate_animation(self):
         """
-        Generate an animation from the saved frames
-        
-        init_func=self.init,
-        blit=True,
-        repeat=False,
+        Generate an animation from the saved frames (matplotlib.figure.Figure
+        objects)
 
-        extra_args=["-vcodec", "libx264"],
+        TODO: it is possible to pass an iterable to frames:
+        e.g. frames=self.frames
         """
         self.ani = animation.FuncAnimation(
-            self.fig,
-            self.update,
-            frames=len(self.frames),
-            init_func=self.init,
-            interval=100,
-            blit=False
+            self.fig, self._update, frames=len(self.frames), interval=100, blit=False
         )
-        self.ani.save(
-            self.name,
-            writer=animation.FFMpegWriter()
-        )
-
-        # i=0
-        # for frame in self.frames:
-        #     print(type(frame))
-        #     frame.savefig(f"{i}-test.png", dpi=300)
-        #     i+=1
+        self.ani.save(self.name, writer=animation.FFMpegWriter())
