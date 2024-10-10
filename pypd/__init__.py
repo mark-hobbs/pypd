cce917447403229837fcead83f460d2987c44dcd
mark-hobbs <markhobbs91@gmail.com>
2024-10-10 15:15:57 +0100

    Add the Triangular influence function

--- pypd/__init__.py
+++ pypd/__init__.py
@@ -6,7 +6,7 @@ from .integrator import EulerCromer
 from .boundary_conditions import BoundaryConditions
 from .material import Material
 from .constitutive_law import Linear, Bilinear, Trilinear, NonLinear
-from .influence import Constant, Quartic
+from .influence import Constant, Quartic, Triangular
 from .penetrator import Penetrator
 from .simulation_data import Observation
 from .animation import Animation
