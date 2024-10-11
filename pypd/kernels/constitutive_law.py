3ca621b762ca4a02505b201f203d3475307a54b5
mark-hobbs <markhobbs91@gmail.com>
2024-10-11 14:20:35 +0100

    Add an index (i) to the function call

--- pypd/kernels/constitutive_law.py
+++ pypd/kernels/constitutive_law.py
@@ -10,17 +10,17 @@ import numpy as np
 
 
 @njit
-def linear(s, d, sc):
+def linear(i, s, d, sc):
     """
     Linear constitutive model
     """
 
     d_temp = 0.0
 
-    if s < sc:
+    if s < sc[i]:
         d_temp = 0.0
 
-    elif s >= sc:
+    elif s >= sc[i]:
         d_temp = 1.0
 
     # Bond softening factor can only increase (damage is irreversible)
@@ -31,21 +31,21 @@ def linear(s, d, sc):
 
 
 @njit
-def trilinear(s, d, s0, s1, sc, beta):
+def trilinear(i, s, d, s0, s1, sc, beta):
     """
     Trilinear constitutive model
     """
 
-    eta = s1 / s0
+    eta = s1[i] / s0
     d_temp = 0.0
 
-    if (s > s0) and (s <= s1):
+    if (s > s0) and (s <= s1[i]):
         d_temp = 1 - ((eta - beta) / (eta - 1) * (s0 / s)) + ((1 - beta) / (eta - 1))
 
-    elif (s > s1) and (s <= sc):
-        d_temp = 1 - (s0 * beta / s) * ((sc - s) / (sc - s1))
+    elif (s > s1[i]) and (s <= sc[i]):
+        d_temp = 1 - (s0 * beta / s) * ((sc[i] - s) / (sc[i] - s1[i]))
 
-    elif s > sc:
+    elif s > sc[i]:
         d_temp = 1.0
 
     # Bond softening factor can only increase (damage is irreversible)
@@ -56,21 +56,21 @@ def trilinear(s, d, s0, s1, sc, beta):
 
 
 @njit
-def nonlinear(s, d, s0, sc, alpha, k):
+def nonlinear(i, s, d, s0, sc, alpha, k):
     """
     Non-linear constitutive model
     """
 
     d_temp = 0.0
 
-    if (s > s0) and (s <= sc):
-        numerator = 1 - np.exp(-k * (s - s0) / (sc - s0))
+    if (s > s0) and (s <= sc[i]):
+        numerator = 1 - np.exp(-k * (s - s0) / (sc[i] - s0))
         denominator = 1 - np.exp(-k)
-        residual = alpha * (1 - (s - s0) / (sc - s0))
+        residual = alpha * (1 - (s - s0) / (sc[i] - s0))
         d_temp = 1 - (s0 / s) * ((1 - (numerator / denominator)) + residual) / (
             1 + alpha
         )
-    elif s > sc:
+    elif s > sc[i]:
         d_temp = 1
 
     if d_temp > d:
