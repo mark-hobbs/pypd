4dd83b249cff61be486a0e2b63dfd2e960928e4c
mark-hobbs <markhobbs91@gmail.com>
2024-10-11 15:18:20 +0100

    Refactoring

--- pypd/constitutive_law.py
+++ pypd/constitutive_law.py
@@ -35,33 +35,16 @@ class ConstitutiveLaw:
     -------
     """
 
-    def __init__(self, t):
-        self.t = t
+    def __init__(self):
+        pass
 
-    def _calculate_bond_stiffness(self, material, particles):
+    def _calculate_sc():
         """
-        Bond stiffness
-            - linear elastic model
-            - 2D
-            - plane stress
-
-        Parameters
-        ----------
-        material :
-            Instance of material class
-
-        Returns
-        -------
-        c : float
-            Bond stiffness
-
-        Notes
-        -----
-        TODO: this function is generic to all material models
+        Calculate the critical stretch
         """
-        return (9 * material.E) / (np.pi * self.t * particles.horizon**3)
+        raise NotImplementedError("This method must be implemented!")
 
-    def calculate_bond_damage():
+    def _calculate_bond_damage():
         """
         Calculate bond damage (softening parameter). The value of d will range
         from 0 to 1, where 0 indicates that the bond is still in the elastic
@@ -84,14 +67,12 @@ class Linear(ConstitutiveLaw):
     -----
     """
 
-    def __init__(self, material, particles, t, c=None, sc=None, damage_on=True):
+    def __init__(self, particles, c, t, sc=None, damage_on=True):
         """
         Linear constitutive model class constructor
 
         Parameters
         ----------
-        material : Material class
-
         particles: ParticleSet class
 
         thickness : float
@@ -111,15 +92,15 @@ class Linear(ConstitutiveLaw):
         * TODO: passing an instance of particles is probably bad design and
         should be improved
         """
+        self.c = c
         self.t = t
-        self.c = c or self._calculate_bond_stiffness(material, particles)
-        self.sc = sc or self._calculate_sc(material, particles)
+        self.sc = self._calculate_sc(particles)
         self.damage_on = damage_on
         self.calculate_bond_damage = self._calculate_bond_damage(
             self.sc, self.damage_on
         )
 
-    def _calculate_sc(self, material, particles):
+    def _calculate_sc(self, particles):
         """
         Calculate the critical stretch for a linear elastic material in
         two-dimensions
@@ -134,7 +115,8 @@ class Linear(ConstitutiveLaw):
         -----
 
         """
-        return np.sqrt((4 * np.pi * material.Gf) / (9 * material.E * particles.horizon))
+        return np.full(len(self.c), np.sqrt((4 * np.pi * particles.material.Gf) 
+                       / (9 * particles.material.E * particles.horizon)))
 
     @staticmethod
     def _calculate_bond_damage(sc, damage_on):
@@ -156,9 +138,8 @@ class Linear(ConstitutiveLaw):
         -----
         """
         if damage_on:
-
             @njit
-            def wrapper(stretch, d):
+            def wrapper(i, stretch, d):
                 """
                 Calculate bond damage
 
@@ -185,12 +166,12 @@ class Linear(ConstitutiveLaw):
                 -----
                 * Examine closures and factory functions
                 """
-                return linear(stretch, d, sc)
+                return linear(i, stretch, d, sc)
 
         else:
 
             @njit
-            def wrapper(stretch, d):
+            def wrapper(i, stretch, d):
                 """
                 Returns
                 -------
@@ -208,14 +189,12 @@ class Bilinear(ConstitutiveLaw):
 
 
 class Trilinear(ConstitutiveLaw):
-    def __init__(self, material, particles, t, c=None, s0=None, sc=None, beta=0.25):
+    def __init__(self, particles, c, t, s0=None, sc=None, beta=0.25):
         """
         Trilinear constitutive model class constructor
 
         Parameters
         ----------
-        material : Material class
-
         particles: ParticleSet class
 
         thickness : float
@@ -240,28 +219,28 @@ class Trilinear(ConstitutiveLaw):
         Notes
         -----
         """
+        self.c = c
         self.t = t
         self.beta = beta
         self.gamma = self._calculate_gamma()
-        self.c = c or self._calculate_bond_stiffness(material, particles)
-        self.s0 = s0 or self._calculate_s0(material)
-        self.sc = sc or self._calculate_sc(material, particles)
+        self.s0 = s0 or self._calculate_s0(particles)
+        self.sc = sc or self._calculate_sc(particles)
         self.s1 = self._calculate_s1()
         self.calculate_bond_damage = self._calculate_bond_damage(
             self.s0, self.s1, self.sc, self.beta
         )
 
-    def _calculate_s0(self, material):
+    def _calculate_s0(self, particles):
         """
         Calculate the linear elastic limit
         """
-        return material.ft / material.E
+        return particles.material.ft / particles.material.E
 
-    def _calculate_sc(self, material, particles):
+    def _calculate_sc(self, particles):
         """
         Trilinear model (2D case) - calculate the critical stretch
         """
-        numerator = 4 * self.gamma * material.Gf
+        numerator = 4 * self.gamma * particles.material.Gf
         denominator = (
             self.t
             * particles.horizon**4
@@ -305,7 +284,7 @@ class Trilinear(ConstitutiveLaw):
         """
 
         @njit
-        def wrapper(stretch, d):
+        def wrapper(i, stretch, d):
             """
             Calculate bond damage
 
@@ -332,7 +311,7 @@ class Trilinear(ConstitutiveLaw):
             -----
             * Examine closures and factory functions
             """
-            return trilinear(stretch, d, s0, s1, sc, beta)
+            return trilinear(i, stretch, d, s0, s1, sc, beta)
 
         return wrapper
 
@@ -348,15 +327,13 @@ class Trilinear(ConstitutiveLaw):
 
 class NonLinear(ConstitutiveLaw):
     def __init__(
-        self, material, particles, t, c=None, s0=None, sc=None, alpha=0.25, k=25
+        self, particles, c, t, s0=None, sc=None, alpha=0.25, k=25
     ):
         """
         Non-linear constitutive model class constructor
 
         Parameters
         ----------
-        material : Material class
-
         particles: ParticleSet class
 
         thickness : float
@@ -383,23 +360,23 @@ class NonLinear(ConstitutiveLaw):
         Notes
         -----
         """
+        self.c = c
         self.t = t
         self.alpha = alpha
         self.k = k
-        self.c = c or self._calculate_bond_stiffness(material, particles)
-        self.s0 = s0 or self._calculate_s0(material)
-        self.sc = sc or self._calculate_sc(material, particles)
+        self.s0 = s0 or self._calculate_s0(particles)
+        self.sc = sc or self._calculate_sc(particles)
         self.calculate_bond_damage = self._calculate_bond_damage(
             self.s0, self.sc, self.alpha, self.k
         )
 
-    def _calculate_s0(self, material):
+    def _calculate_s0(self, particles):
         """
         Calculate the linear elastic limit
         """
-        return material.ft / material.E
+        return particles.material.ft / particles.material.E
 
-    def _calculate_sc(self, material, particles):
+    def _calculate_sc(self, particles):
         """
         Nonlinear model (2D case) - calculate the critical stretch
         """
@@ -416,7 +393,7 @@ class NonLinear(ConstitutiveLaw):
                 - (self.alpha * self.k * np.exp(self.k) + 2)
             )
         ) / ((4 * self.k) + (np.exp(self.k) - 1) * (1 + self.alpha))
-        numerator = numerator_a * (material.Gf - numerator_b)
+        numerator = numerator_a * (particles.material.Gf - numerator_b)
         denominator_a = self.t * self.c * particles.horizon**4 * self.s0
         denominator_b = (
             (2 * self.k)
@@ -458,7 +435,7 @@ class NonLinear(ConstitutiveLaw):
         """
 
         @njit
-        def wrapper(stretch, d):
+        def wrapper(i, stretch, d):
             """
             Calculate bond damage
 
@@ -485,7 +462,7 @@ class NonLinear(ConstitutiveLaw):
             -----
             * Examine closures and factory functions
             """
-            return nonlinear(stretch, d, s0, sc, alpha, k)
+            return nonlinear(i, stretch, d, s0, sc, alpha, k)
 
         return wrapper
 
