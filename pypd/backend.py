611d6fbddd2a63886693f34f322c3e8630ae24ed
mark-hobbs <markhobbs91@gmail.com>
2026-02-26 17:11:20 +0000

    Add typehints

--- pypd/backend.py
+++ pypd/backend.py
@@ -1,8 +1,15 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from collections.abc import Callable
+
 from numba import cuda
 from .kernels.particles import (
     make_compute_nodal_forces_cpu,
     make_compute_nodal_forces_gpu,
 )
+
+if TYPE_CHECKING:
+    from .model import Model
 
 
 class Backend:
@@ -10,9 +17,12 @@ class Backend:
     Backend class - handle device logic (GPU/CPU)
     """
 
-    def __init__(self, model):
-        self.model = model
-        self.cuda_available = cuda.is_available()
+    def __init__(self, model: Model) -> None:
+        self.model: Model = model
+        self.cuda_available: bool = cuda.is_available()
+        self.compute_particle_forces_cpu: Callable[..., None] | None = None
+        self.compute_particle_forces_gpu: Callable[..., None] | None = None
+
         print(f"Is CUDA available: {self.cuda_available}")
 
         if self.cuda_available:
@@ -27,13 +37,13 @@ class Backend:
                 # self.model.penetrators.compile_cpu()
                 pass
 
-    def _build_force_function_cpu(self):
+    def _build_force_function_cpu(self) -> None:
         self.model.bonds.constitutive_law.compile_cpu()
         self.compute_particle_forces_cpu = make_compute_nodal_forces_cpu(
             self.model.bonds.constitutive_law.calculate_bond_damage
         )
 
-    def _build_force_function_gpu(self):
+    def _build_force_function_gpu(self) -> None:
         """
         material_law = make_material_law(sc)
         compute_nodal_forces_kernel = make_compute_nodal_forces_kernel(material_law)
@@ -43,12 +53,12 @@ class Backend:
             self.model.bonds.constitutive_law.calculate_bond_damage
         )
 
-    def host_to_device(self):
+    def host_to_device(self) -> None:
         if self.cuda_available:
             self.model.particles._host_to_device()
             self.model.bonds._host_to_device()
 
-    def device_to_host(self):
+    def device_to_host(self) -> None:
         if self.cuda_available:
             self.model.particles._device_to_host()
             self.model.bonds._device_to_host()
@@ -71,6 +81,7 @@ class Backend:
         * Particle forces are modified in place
         """
         if self.cuda_available:
+            assert self.compute_particle_forces_gpu is not None
             self.compute_particle_forces_gpu(
                 self.model.particles.d_f,
                 self.model.particles.d_x,
@@ -85,6 +96,7 @@ class Backend:
                 self.model.bonds.d_sc,
             )
         else:
+            assert self.compute_particle_forces_cpu is not None
             self.compute_particle_forces_cpu(
                 self.model.particles.f,
                 self.model.particles.x,
